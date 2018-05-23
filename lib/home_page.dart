d478417533d435d02d285be89519de402c48ff0e
laileon <leon@igola.com>
2018-05-23 18:18:46 +0800

    drawer

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,9 +4,17 @@ import 'dart:io';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:news_aggreator/drawer_page.dart';
 import 'package:news_aggreator/post.dart';
 import 'package:news_aggreator/post_details.dart';
 import 'package:news_aggreator/strings.dart';
+
+class DrawerItem {
+  String title;
+  IconData icon;
+
+  DrawerItem(this.title, this.icon);
+}
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -22,6 +30,12 @@ class MyHomePage extends StatefulWidget {
 
   final String title;
 
+  final drawerItems = [
+    new DrawerItem(Strings.appName, Icons.rss_feed),
+    new DrawerItem("Fragment 2", Icons.local_pizza),
+    new DrawerItem("Fragment 3", Icons.info)
+  ];
+
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
@@ -30,6 +44,45 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _isRequestSent = false;
   bool _isRequestFailed = false;
   List<Post> postList = [];
+
+  String currentProfilePic =
+      "https://avatars3.githubusercontent.com/u/16825392?s=460&v=4";
+  String otherProfilePic =
+      "https://yt3.ggpht.com/-2_2skU9e2Cw/AAAAAAAAAAI/AAAAAAAAAAA/6NpH9G8NWf4/s900-c-k-no-mo-rj-c0xffffff/photo.jpg";
+
+  void switchAccounts() {
+    String picBackup = currentProfilePic;
+    this.setState(() {
+      currentProfilePic = otherProfilePic;
+      otherProfilePic = picBackup;
+    });
+  }
+
+  int _selectedDrawerIndex = 0;
+
+  _onSelectItem(int index) {
+    setState(() => _selectedDrawerIndex = index);
+    Navigator.of(context).pop(); // close the drawer
+    if (index > 0)
+      Navigator.of(context).push(new PageRouteBuilder(
+          opaque: false,
+          pageBuilder: (BuildContext context, _, __) {
+            return new Page("Page " + index.toString());
+          },
+          transitionsBuilder:
+              (_, Animation<double> animation, __, Widget child) {
+            return new FadeTransition(
+              opacity: animation,
+              child: new SlideTransition(
+                  position: new Tween<Offset>(
+                    begin: const Offset(0.0, 1.0),
+                    end: Offset.zero,
+                  )
+                      .animate(animation),
+                  child: child),
+            );
+          }));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,18 +95,59 @@ class _MyHomePageState extends State<MyHomePage> {
     if (!_isRequestSent) {
       sendRequest();
     }
+
+    List<Widget> drawerOptions = [];
+    for (var i = 0; i < widget.drawerItems.length; i++) {
+      var d = widget.drawerItems[i];
+      drawerOptions.add(new ListTile(
+        leading: new Icon(d.icon),
+        title: new Text(d.title),
+        selected: i == _selectedDrawerIndex,
+        onTap: () => _onSelectItem(i),
+      ));
+    }
+
     return new Scaffold(
       appBar: new AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text(widget.drawerItems[_selectedDrawerIndex].title),
+      ),
+      drawer: new Drawer(
+        child: new Column(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountEmail: new Text("bramvbilsen@gmail.com"),
+              accountName: new Text("Bramvbilsen"),
+              currentAccountPicture: new GestureDetector(
+                child: new CircleAvatar(
+                  backgroundImage: new NetworkImage(currentProfilePic),
+                ),
+                onTap: () => print("This is your current account."),
+              ),
+              otherAccountsPictures: <Widget>[
+                new GestureDetector(
+                  child: new CircleAvatar(
+                    backgroundImage: new NetworkImage(otherProfilePic),
+                  ),
+                  onTap: () => switchAccounts(),
+                ),
+              ],
+              decoration: new BoxDecoration(
+                  image: new DecorationImage(
+                      image: new NetworkImage(
+                          "https://img00.deviantart.net/35f0/i/2015/018/2/6/low_poly_landscape__the_river_cut_by_bv_designs-d8eib00.jpg"),
+                      fit: BoxFit.fill)),
+            ),
+            new Column(children: drawerOptions)
+          ],
+        ),
       ),
       body: new Container(
         alignment: Alignment.center,
         child: !_isRequestSent
             //  Request has not been sent, let's show a progress indicator
             ? new CircularProgressIndicator()
-
             // Request has been sent but did it fail?
             : _isRequestFailed
                 // Yes, it has failed. Show a retry UI
